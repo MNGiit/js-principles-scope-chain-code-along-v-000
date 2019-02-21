f96be4de1b96130582014c3b03d14e00781722ff
Mohammed Noori <kmanoorib@gmail.com>
2019-02-21 03:08:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,19 +13,16 @@ function overWriteBestCustomer() {
   return bestCustomer = "maybe bob";
 }
 
+const leastFavoriteCustomer = "no one";
+
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = "still no one";
+}
 
 
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = bestCustomer;
+  let favoriteCustomer = "someone";
+  return favoriteCustomer;
+}
 
-overWriteBestCustomer()
-overwrites the best customer ‣
-ReferenceError: overWriteBestCustomer is not defined
-    at Context.it (test/indexTest.js:26:7)overWriteBestCustomer('maybe bob')
-expect(window.bestCustomer).to.equal('maybe bob')
-reassignLeastFavorite
-unsuccessfully tries to reassign the least favorite customer ‣
-ReferenceError: changeLeastFavoriteCustomer is not defined
-    at Context.it (test/indexTest.js:33:14)expect(changeLeastFavoriteCustomer).to.throw(TypeError)
-attemptTwoFavoriteCustomers
-unsuccessfully tries to declare favoriteCustomer with let two times ‣
-ReferenceError: attemptTwoFavoriteCustomers is not defined
-    at Context.it (test/indexTest.js:39:14)expect(attemptTwoFavoriteCustomers).to.throw(SyntaxError)
